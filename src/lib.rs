a2bde647c0870ea1c605fffe1a801c92f8c7018d
Casey Rodarmor <casey@rodarmor.com>
2020-02-13 00:37:23 -0800

    Show error

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,16 @@ mod submodule {
     mod tests {
         extern crate serde_ as serde;
 
-        use serde_derive::{Deserialize, Serialize};
+        use serde::{Deserialize, Serialize};
+
         #[test]
         fn foo() {
+            // error[E0463]: can't find crate for `serde`
+            //   --> src/lib.rs:15:40
+            //    |
+            // 15 |             #[derive(Debug, Serialize, Deserialize, PartialEq)]
+            //    |                                        ^^^^^^^^^^^ can't find crate
+
             #[derive(Debug, Serialize, Deserialize, PartialEq)]
             struct Foo {}
         }
